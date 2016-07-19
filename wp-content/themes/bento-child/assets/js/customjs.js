7d6d358ec255ca4656ee2d7ea194903a264e7260
Anna <annaxantzop@yahoo.gr>
2016-07-19 12:47:03 +0300

    Template changes

---
+++ wp-content/themes/bento-child/assets/js/customjs.js
@@ -0,0 +1,33 @@
+
+jQuery(document).ready(function($) {
+  "use strict";
+
+  // Display the copyright in the footer
+	$(".footer-copyright").html("© 2016 "+"<a href='http://genesis-paedeia.org/' target='_blank'>"+"www.genesis-paedeia.org</a> - "
+		+"<a href='http://www.hyphensa.com/site/index.php/en/' target='_blank' >hyphen SA</a>");
+
+  // Create back-to-top button
+  var html = "<a href='#' class='back-to-top' style='display: inline;'><i class='fa fa-arrow-circle-up'></i></a>";
+  $(".footer-copyright").append(html);
+
+  // back-to-top button scroll
+  var offset = 250;
+  var duration = 300;
+  jQuery(window).scroll(function() {
+    if (jQuery(this).scrollTop() > offset) {
+      jQuery('.back-to-top').fadeIn(duration);
+    } else {
+      jQuery('.back-to-top').fadeOut(duration);
+    }
+  });
+
+  jQuery('.back-to-top').click(function(event) {
+    event.preventDefault();
+    jQuery('html, body').animate({scrollTop: 0}, duration);
+    return false;
+  });
+
+
+  
+
+});
---
+++ wp-content/themes/bento-child/functions.php
@@ -0,0 +1,23 @@
+<?php
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'parent-style';
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style )
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+function my_scripts_method() {
+    wp_enqueue_script(
+        'customjs',
+        get_stylesheet_directory_uri() . '/assets/js/customjs.js',
+        array( 'jquery' )
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
+
+?>
