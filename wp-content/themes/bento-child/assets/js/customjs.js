018b33d05288ce7f487d259900487d798e32c0ae
Anna <annaxantzop@yahoo.gr>
2016-07-25 12:17:28 +0300

    ngh

--- wp-content/themes/bento-child/assets/js/customjs.js
+++ wp-content/themes/bento-child/assets/js/customjs.js
@@ -6,12 +6,11 @@ jQuery(document).ready(function($) {
 	$(".footer-copyright").html("© 2016 "+"<a href='http://genesis-paedeia.org/' target='_blank'>"+"www.genesis-paedeia.org</a> - "
 		+"<a href='http://www.hyphensa.com/site/index.php/en/' target='_blank' >hyphen SA</a>");
 
-
   // Create back-to-top button
-  var html = "<a href='#' class='back-to-top' style='display: inline;'><i class='fa fa-arrow-circle-up'></i></a>";
-  $(".footer-copyright").append(html);
+  var top = "<a href='#' class='back-to-top' style='display: inline;'><i class='fa fa-arrow-circle-up'></i></a>";
+  $(".footer-copyright").append(top);
 
-  // back-to-top button scroll
+  // Back-to-top button scroll
   var offset = 250;
   var duration = 300;
   jQuery(window).scroll(function() {
@@ -28,6 +27,17 @@ jQuery(document).ready(function($) {
     return false;
   });
 
+  // Blog title added href
+  var link = "<a href='http://localhost/genesis-paedeia/blog' target='_blank' style='display: inline;text-decoration:none;'>Blog</a>"
+  jQuery('.blog-section').find('.widget-title').html(link);
+
+  // Screen size .post-header
+  var screenheight = screen.height;
+  jQuery('.post-header').height(screenheight);
+
+  // scroll down icon
+  var down = "<div class='arrow-down'><a href='#three' class=''><i class='fa fa-chevron-down'></i></a></div>";
+  jQuery('.post-header-title').append(down);
 
 
 
